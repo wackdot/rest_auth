43c4611aa3283c85c10c7f8e9e781ac60db84df7
wackdot <e0167324@u.nus.edu>
2017-12-28 17:43:09 +0800

    28/12/17 5:43 PM

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('password', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'password', 'first_name', 'last_name', 'email',)
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
  
